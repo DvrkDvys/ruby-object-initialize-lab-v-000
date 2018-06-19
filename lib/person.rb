86218548e9042aa6072d7acca5b816d2080daca3
Jordan Harris <jharris@wework.com>
2018-06-19 18:10:30 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -6,13 +6,13 @@ class Person
   def name=beyonce
     @name = beyonce
   end
+  
+  def name 
+    @name
+  end
 
   def job=(singer)
     @job = singer
-  end
-
-  def name 
-    @name
   end
 end
 
