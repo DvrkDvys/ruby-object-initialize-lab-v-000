c488f13615656724389a2516b53cf75ff47e0e65
Jordan Harris <jharris@wework.com>
2018-06-19 18:21:31 +0000

    automatically backed up by learn

--- spec/object_initialize_spec.rb
+++ spec/object_initialize_spec.rb
@@ -11,7 +11,7 @@ end
 describe 'Dog' do
   describe '#initialize' do
     it 'sets the name of the dog in an instance variable @name' do
-      fido = Dog.new("Fido", "Pug")
+      fido = Dog.new("Fido")
 
       expect(fido.instance_variable_get(:@name)).to eq("Fido")
     end
