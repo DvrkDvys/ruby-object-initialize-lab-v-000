0f25abe24ede70c6dbec531551ea5a07c623ec0e
Jordan Harris <jharris@wework.com>
2018-06-19 18:13:59 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -21,27 +21,27 @@ class Dog
   end
 end
 
-beyonce = Dog.new("Fido")
+fido = Dog.new("Fido")
 
-fido = Dog.new
-fido.name = "Fido"
+# fido = Dog.new
+# fido.name = "Fido"
 
-snoopy = Dog.new
-snoopy.breed = "Beagle" 
+# snoopy = Dog.new
+# snoopy.breed = "Beagle" 
 
-  it 'sets the name of the dog in an instance variable @name' do
-      fido = Dog.new("Fido", "Pug")
+  # it 'sets the name of the dog in an instance variable @name' do
+  #     fido = Dog.new("Fido", "Pug")
 
-      expect(fido.instance_variable_get(:@name)).to eq("Fido")
-    end
+  #     expect(fido.instance_variable_get(:@name)).to eq("Fido")
+  #   end
 
-    it 'sets the breed of the dog in an instance variable @breed' do
-      fido = Dog.new("Fido", "Pug")
+  #   it 'sets the breed of the dog in an instance variable @breed' do
+  #     fido = Dog.new("Fido", "Pug")
 
-      expect(fido.instance_variable_get(:@breed)).to eq("Pug")
-    end
+  #     expect(fido.instance_variable_get(:@breed)).to eq("Pug")
+  #   end
 
-    it 'defaults the breed argument to "Mutt" in an instance variable @breed' do
-      fido = Dog.new("Fido")
+  #   it 'defaults the breed argument to "Mutt" in an instance variable @breed' do
+  #     fido = Dog.new("Fido")
 
-      expect(fido.instance_variable_get(:@breed)).to eq("Mutt")
+  #     expect(fido.instance_variable_get(:@breed)).to eq("Mutt")
